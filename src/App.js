7d9d36945b5db71ffd10f5cf6b8a8a7675ad9d27
osugi001 <102548267+osugi001@users.noreply.github.com>
2024-02-03 16:35:50 -0800

    Updated App.js
    Imported Coinflip component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Home from "./pages/Home";
+import Home from './pages/Home';
+import Coinflip from './pages/Coinflip'; // Import Coinflip component
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/coinflip" element={<Coinflip />} /> {/* Add Coinflip route */}
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;

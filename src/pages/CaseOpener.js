22e29310cd04374c8e0da5e6c0e74c14bbe3d68f
Ryan Hernandez <86322787+CoolGuyRy@users.noreply.github.com>
2024-02-15 04:24:44 -0800

    Changed the Case Opener around.

--- src/pages/CaseOpener.js
+++ src/pages/CaseOpener.js
@@ -1,98 +1,141 @@
 import React from 'react';
 import './CaseOpener.css';
 
-function ItemCard({ itemName, itemURL, itemWear, itemPrice }) {
+function ItemCard({ itemName, itemURL, itemRarity }) {
+
+  let itemColor = '';
+  switch (itemRarity) {
+    case 'Consumer':
+      itemColor = '#afafaf';
+      break;
+    case 'Industrial':
+      itemColor = '#6496e1';
+      break;
+    case 'Mil-Spec':
+      itemColor = '#4b69cd';
+      break;
+    case 'Restricted':
+      itemColor = '#8847ff';
+      break;
+    case 'Classified':
+      itemColor = '#d32ce6';
+      break;
+    case 'Covert':
+      itemColor = '#eb4b4b';
+      break;
+    case 'Contraband':
+      itemColor = '#886a08';
+      break;
+    default:
+      itemColor = '#ffffff';
+      break;
+  }
+
   return (
     <div className='ItemCard'>
       <div className='ItemCard2'>
         <h1 style={{
-          color: 'white',
-          textAlign: 'center'
+          color: itemColor,
+          textAlign: 'center',
+          padding: '15px'
         }}>
           {itemName}
         </h1>
-        <h2 style={{
-          color: 'white',
-          textAlign: 'center'
-        }}>
-          {itemWear}
-        </h2>
         <img 
           src={itemURL} 
           alt={itemName} 
         />
-        <h3 style={{
-          color: 'white',
-          textAlign: 'center'
-        }}>
-          {itemPrice}
-        </h3>
       </div>
     </div>
   );
 }
 
-let userItems = [
+let Chroma2Case = [
+  {
+    'ItemName'  : "AK-47 | Elite Build",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_217.jpg",
+    'ItemRarity': 'Mil-Spec'
+  },
+  {
+    'ItemName'  : "MP7 | Armor Core",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_223.jpg",
+    'ItemRarity': 'Mil-Spec'
+  },
+  {
+    'ItemName'  : "Desert Eagle | Bronze Deco",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_219.jpg",
+    'ItemRarity': 'Mil-Spec'
+  },
+  {
+    'ItemName'  : "P250 | Valence",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_221.jpg",
+    'ItemRarity': 'Mil-Spec'
+  },
+  {
+    'ItemName'  : "Negev | Man-o'-war",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_227.jpg",
+    'ItemRarity': 'Mil-Spec'
+  },
+  {
+    'ItemName'  : "Sawed-Off | Origami",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_225.jpg",
+    'ItemRarity': 'Mil-Spec'
+  },
+  {
+    'ItemName'  : "AWP | Worm God",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_209.jpg",
+    'ItemRarity': 'Restricted'
+  },
+  {
+    'ItemName'  : "MAG-7 | Heat",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_211.jpg",
+    'ItemRarity': 'Restricted'
+  },
+  {
+    'ItemName'  : "CZ75-Auto | Pole Position",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_213.jpg",
+    'ItemRarity': 'Restricted'
+  },
+  {
+    'ItemName'  : "UMP-45 | Grand Prix",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_215.jpg",
+    'ItemRarity': 'Restricted'
+  },
+  {
+    'ItemName'  : "Five-SeveN | Monkey Business",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_205.jpg",
+    'ItemRarity': 'Classified'
+  },
+  {
+    'ItemName'  : "Galil AR | Eco",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_203.jpg",
+    'ItemRarity': 'Classified'
+  },
+  {
+    'ItemName'  : "FAMAS | Djinn",
+    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_207.jpg",
+    'ItemRarity': 'Classified'
+  },
   {
     'ItemName'  : "M4A1-S Hyper Beast",
     'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_199.jpg",
-    'ItemWear'  : 'Factory New',
-    'ItemPrice' : '$146.00'
+    'ItemRarity': 'Covert'
   },
   {
-    'ItemName'  : "MAC-10 Neon Rider",
+    'ItemName'  : "MAC-10 | Neon Rider",
     'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_201.jpg",
-    'ItemWear'  : 'Minimal Wear',
-    'ItemPrice' : '$7.10'
+    'ItemRarity': 'Covert'
   },
-  {
-    'ItemName'  : "Galil AR Eco",
-    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_203.jpg",
-    'ItemWear'  : 'Field Tested',
-    'ItemPrice' : '$4.71'
-  },
-  {
-    'ItemName'  : "FAMAS Djinn",
-    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_207.jpg",
-    'ItemWear'  : 'Factory New',
-    'ItemPrice' : '$11.06'
-  },
-  {
-    'ItemName'  : "Five-SeveN Monkey Business",
-    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_205.jpg",
-    'ItemWear'  : 'Minimal Wear',
-    'ItemPrice' : '$11.40'
-  },
-  {
-    'ItemName'  : "AWP Worm God",
-    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_209.jpg",
-    'ItemWear'  : 'Factory New',
-    'ItemPrice' : '$7.91'
-  },
-  {
-    'ItemName'  : "MAG-7 Heat",
-    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_211.jpg",
-    'ItemWear'  : 'Battle Scarred',
-    'ItemPrice' : '$0.26'
-  },
-  {
-    'ItemName'  : "AK-47 Elite Build",
-    'ItemURL'   : "/assets/Case Item Images/Chroma-2-Case/image_217.jpg",
-    'ItemWear'  : 'Minimal Wear',
-    'ItemPrice' : '$1.89'
-  }
-];
+]
 
 function CaseOpener() {
   return (
     <div>
-      {userItems.map(item => (
+      {Chroma2Case.map(item => (
         <ItemCard
-          key={item.ItemName} // Using the item name as a key; ensure it's unique
           itemName={item.ItemName}
           itemURL={item.ItemURL}
-          itemWear={item.ItemWear}
-          itemPrice={item.ItemPrice}
+          itemRarity={item.ItemRarity}
         />
       ))}
     </div>

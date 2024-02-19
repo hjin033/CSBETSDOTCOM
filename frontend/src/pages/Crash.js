2c29c190d8bd6134c11e97848673471533f1db41
jacobmooradian <jacobmooradian@gmail.com>
2024-02-19 13:07:16 -0800

    reorganizing project to create separate directories for frontend and backend. launching frontend, just navigate to the frontend directory and use npm start

---
+++ frontend/src/pages/Crash.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+
+function Crash() {
+  return (
+    <div>
+      <Navbar />
+      <h2>CRASH GAME</h2>
+    </div>
+  );
+}
+
+export default Crash;

525dada1ae9c1e682f955593392800d10b07b9fc
jacobmooradian <jacobmooradian@gmail.com>
2024-03-02 22:09:49 -0800

    user auth works, populates db with new users when you register and should be able to login. to start backend, you need to run node app.js while in backend directory. run parallel with frontend.

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,44 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const bcrypt = require('bcrypt');
+const pool = require('./db');
 const cors = require('cors');
-require('dotenv').config();
-
-const userRoutes = require('./routes/users'); 
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
-app.use(express.json()); 
 
-app.use('/api/users', userRoutes);
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+    } else {
+        console.log('Database connection successful:', res.rows[0]);
+    }
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
 
-module.exports = app;
+app.use('/api/auth', authRoutes);
+
+app.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10); 
+        const newUser = await pool.query(
+            'INSERT INTO users ("username", "password", "balance", "isAdmin") VALUES ($1, $2, $3, $4) RETURNING *',
+            [username, hashedPassword, 100, false]
+        );
+
+        res.json(newUser.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+app.listen(4000, () => {
+    console.log('Server started on port 4000');
+});
